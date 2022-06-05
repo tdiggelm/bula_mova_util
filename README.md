08c13bc424f3b8224bf7ada6a1d318313962a17b
Thomas Diggelmann <tdiggelm@gmail.com>
2022-06-05 11:52:53 +0200

    changed deviceIds -> _deviceId; by default merge all exports into output_path xlsx file, option for doing in splitting manner

--- README.md
+++ README.md
@@ -15,8 +15,20 @@ pip install -r requirements.txt
 
 ## Export personal events
 
+Export everything into a single files xlsx file:
+
 ```shell
 python scripts/export-events-per-person.py \
+    "./data/Staff Planning - master for DB support.xlsx" \
+    "./data/App-Users mit device-ID.xlsx" \
+    "./outputs/all_events_merged.xlsx"
+```
+
+Export into individual xlsx files (named with the `profile_id`, e.g. `./outputs/events_per_person/{profile_id}.xlsx`):
+
+```shell
+python scripts/export-events-per-person.py \
+    --split-exports \
     "./data/Staff Planning - master for DB support.xlsx" \
     "./data/App-Users mit device-ID.xlsx" \
     "./outputs/events_per_person"
--- scripts/export-events-per-person.py
+++ scripts/export-events-per-person.py
@@ -2,9 +2,10 @@ import argparse
 import pandas as pd
 import os
 from tqdm.autonotebook import tqdm
+from itertools import chain
 
 
-def export(input_filename, app_users_filename, output_dir):
+def export(input_filename, app_users_filename, output_path, split_exports):
     checks_start_col = 3
     header_rows = slice(2, 8)
     header_cols = slice(4, None)
@@ -13,7 +14,8 @@ def export(input_filename, app_users_filename, output_dir):
 
     df_main = pd.read_excel(input_filename)
     df_devices = pd.read_excel(app_users_filename)
-    os.makedirs(output_dir, exist_ok=True)
+    if split_exports:
+        os.makedirs(output_path, exist_ok=True)
 
     device_ids = {}
     for _, row in df_devices.iterrows():
@@ -27,7 +29,8 @@ def export(input_filename, app_users_filename, output_dir):
         event_ids_per_day.append([event for event in row if pd.notna(event)])
 
     body_data = df_main.iloc[body_rows, body_cols]
-    for row in tqdm(body_data.itertuples(index=False), desc="Exporting files"):
+    all_records = []
+    for row in tqdm(body_data.itertuples(index=False), desc="Exporting data"):
         name, email, profile_id = row[0:3]
         all_event_ids = []
         for event_ids, check in zip(event_ids_per_day, row[checks_start_col:]):
@@ -41,14 +44,24 @@ def export(input_filename, app_users_filename, output_dir):
                     "E-Mail": email,
                     "Profil-ID": profile_id,
                     "TerminId": event_id,
-                    "deviceIds": device_ids[profile_id],
+                    "_deviceId": device_ids[profile_id],
                 }
             )
-        personal_df = pd.DataFrame.from_records(
-            records, columns=["Name", "E-Mail", "Profil-ID", "TerminId", "deviceIds"]
+        all_records.append((profile_id, records))
+
+    columns = ["Name", "E-Mail", "Profil-ID", "TerminId", "_deviceId"]
+    
+    if split_exports:
+        for profile_id, records in all_records:
+            personal_df = pd.DataFrame.from_records(records, columns=columns)
+            output_filename = os.path.join(output_path, f"{profile_id}.xlsx")
+            personal_df.to_excel(output_filename, index=False)
+    else:
+        merged_records = chain.from_iterable(
+            records for _, records in all_records
         )
-        output_filename = os.path.join(output_dir, f"{profile_id}.xlsx")
-        personal_df.to_excel(output_filename)
+        merged_df = pd.DataFrame.from_records(merged_records, columns=columns)
+        merged_df.to_excel(output_path, index=False)
 
 
 def get_args():
@@ -57,17 +70,27 @@ def get_args():
         "input_filename", help="The main file containing all personal and events"
     )
     parser.add_argument(
-        "app_users_filename", help="Contains a table with users and deviceids"
+        "input_app_users_filename", help="Contains a table with users and deviceids"
     )
     parser.add_argument(
-        "output_dir", help="The target directory for the indivudual exports"
+        "output_path", help="The target output path: a **directory** that is used for outputting individial xlsx exports for each person if flag `--split_exports is set, otherwise an **filename** where the entire export is written to as xlsx"
+    )
+    parser.add_argument(
+        "--split-exports",
+        action="store_true",
+        help="writes individual outputs for each person if set, otherwise merges all outputs into single file"
     )
     return parser.parse_args()
 
 
 def main():
     args = get_args()
-    export(args.input_filename, args.app_users_filename, args.output_dir)
+    export(
+        args.input_filename,
+        args.input_app_users_filename,
+        args.output_path,
+        args.split_exports,
+    )
 
 
 if __name__ == "__main__":
